37d3d7b138eb3160ab1f7e2ab2fe3b22dd9ebae0
Anderson kich <andersonkichdev@gmail.com>
2021-07-28 10:26:55 -0300

    First commit

---
+++ src/fonts/index.ts
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from 'styled-components';
+
+import inter_400 from './Inter/inter-400.ttf'
+import inter_600 from './Inter/inter-600.ttf'
+
+import lexend_400 from './Lexend/lexend-400.ttf'
+import lexend_500 from './Lexend/lexend-500.ttf'
+import lexend_600 from './Lexend/lexend-600.ttf'
+
+export default createGlobalStyle`
+    
+    @font-face {
+        font-family: Inter_400;
+        font-style: normal;
+        font-weight: 400;
+        src: url(${inter_400});  
+    }
+    @font-face {
+        font-family: Inter_600;
+        font-style: normal;
+        font-weight: 600;
+        src: url(${inter_600});  
+    }
+    @font-face {
+        font-family: Lexend_400;
+        font-style: normal;
+        font-weight: 400;
+        src: url(${lexend_400});  
+    }
+    @font-face {
+        font-family: Lexend_500;
+        font-style: normal;
+        font-weight: 500;
+        src: url(${lexend_500});  
+    }
+    @font-face {
+        font-family: Lexend_600;
+        font-style: normal;
+        font-weight: 600;
+        src: url(${lexend_600});  
+    }
+`;
---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+
+* {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+  @media (max-width: 1080px) {
+    html {
+      font-size: 93.75%;
+    }   
+  }
+  @media (max-width: 720px) {
+    html {
+      font-size: 75%;
+    }   
+  }
+  h1, h2, h3, h4, h5, h6 {
+    font-family: Lexend_600, sans-serif;  
+  }
+  ul{
+    list-style: none;
+  }
+  p, input, textarea, button {
+    font-family: Inter_400;
+  }
+  p {
+    font-size: 1rem;
+  }
+  h1 {
+    font-size: 2rem;
+  }
+  h2 {
+      font-size: 1.5rem;
+  }
+  button {
+      cursor: pointer;
+  }
+  a:link 
+  { 
+    text-decoration:none; 
+  } 
+
+`; 
